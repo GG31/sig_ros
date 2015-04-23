255fad5a4ed2d6dcb3bcec3771d550b37909cf2c
Ubuntu 12.04 <gg@ubuntu.(none)>
2015-04-24 08:08:30 +0900

    README + xml

---
+++ README.md
@@ -0,0 +1,9 @@
+sig_ros is a package to interface the SIGverse world with ROS. Indeed, with this package you will be able to use SIGverse through ROS.
+user is an example of how to use the sig_ros package. It's the clean up task.
+
+1. Put sig_ros and user in the src folder of your workspace.
+2. Change the absolute links on user/xml/CleanUpDemo2014.xml (5), sig_ros/src/ros_controller.cpp (1)
+3. Go to user/xml directory
+4. Run : rosrun sig_ros ros_Controller
+5. Start simulation
+6. Run : rosrun user RobotCommand
--- sig_ros/src/ros_controller.cpp
+++ sig_ros/src/ros_controller.cpp
@@ -12,31 +12,6 @@
 #define KEYCODE_D 0x42
 #define KEYCODE_Q 0x71
 
-/*class CommandSig
-{
-public:
-   CommandSig();
-   void CommandLoop();
-   void chatterCallback (const std_msgs::String::ConstPtr& msg);
-private:
-   ros::NodeHandle nh_;
-   double linear_, angular_, l_scale_, a_scale_;
-   ros::Publisher vel_pub_;
-   ros::Subscriber  mess_sub ;
-};
-
-CommandSig::CommandSig():
-  linear_(0),
-  angular_(0),
-  l_scale_(2.0),
-  a_scale_(2.0)
-{
-   nh_.param("scale_angular", a_scale_, a_scale_);
-   nh_.param("scale_linear", l_scale_, l_scale_);
-   vel_pub_ = nh_.advertise<sig_ros::Velocity>("mycommand_velocity", 1);
-   mess_sub = nh_.subscribe("message_command",100,&CommandSig::chatterCallback,this);
-}*/
-
 void chatterCallback(const std_msgs::String::ConstPtr& msg)
 {
    ROS_INFO("I heard: [%s]", msg->data.c_str());
@@ -55,132 +30,11 @@ int main(int argc, char** argv)
    // Put them in the xml file
    // Compile all controller
    // Launch sigserver
-   std::string command = "sigserver.sh -w /home/gg/catkin_ws/devel/lib/libsig_ros/CleanUpDemo2014Robo.xml";// + std::string(argv[1]);// devel/lib/libsig_ros/RobotInMove.xml
+   std::string command = "sigserver.sh -w /home/gg/catkin_ws/src/user/xml/CleanUpDemo2014Robo.xml";// Place of CleaUpDemo2014Robo.xml 
+   //+ std::string(argv[1]);// devel/lib/libsig_ros/RobotInMove.xml
    system (command.c_str());
    
    //ros::Rate loop_rate(100);
    //ros::spin();
    return(0);
 }
-
-
-/*void CommandSig::chatterCallback (const std_msgs::String::ConstPtr& msg)
-{
-   sig_ros::Velocity vel;
-   if(strcmp("moveforward",msg->data.c_str())==0)
-   {
-      vel.angular = 3.0;
-      vel.linear = 3.0;
-      vel_pub_.publish(vel);
-   }
-   else if(strcmp("movebackward",msg->data.c_str())==0)
-   {
-      vel.angular = -3.0;
-      vel.linear = -3.0;
-      vel_pub_.publish(vel);
-   }
-   else if(strcmp("turnleft",msg->data.c_str())==0)
-   {
-      vel.angular = 0.78;
-      vel.linear = -0.78;
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = 0.0;
-      vel.linear =  0.0;
-      vel_pub_.publish(vel);
-   }
-   else if(strcmp("turnright",msg->data.c_str())==0)
-   {
-      vel.angular = -0.78;
-      vel.linear = 0.78;
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = 0.0;
-      vel.linear =  0.0;
-      vel_pub_.publish(vel);
-   }
-   else if(strcmp("stop",msg->data.c_str())==0)
-   {
-      vel.angular = 0.0;
-      vel.linear = 0.0;
-      vel_pub_.publish(vel);
-   }
-   else if(strcmp("square",msg->data.c_str())==0)
-   {
-      vel.angular = 3.0;
-      vel.linear = 3.0;
-
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = -0.78;
-      vel.linear = 0.78;
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = 0.0;
-      vel.linear =  0.0;
-      vel_pub_.publish(vel);
-      vel.angular = 3.0;
-      vel.linear = 3.0;
-
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = -0.78;
-      vel.linear = 0.78;
-
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = 0.0;
-      vel.linear =  0.0;
-
-      vel_pub_.publish(vel);
-      vel.angular = 3.0;
-      vel.linear = 3.0;
-
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = -0.78;
-      vel.linear = 0.78;
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = 0.0;
-      vel.linear =  0.0;
-      vel_pub_.publish(vel);
-
-      vel.angular = 3.0;
-      vel.linear = 3.0;
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = -0.78;
-      vel.linear = 0.78;
-      vel_pub_.publish(vel);
-      sleep(1.8);
-      vel.angular = 0.0;
-      vel.linear =  0.0;
-      vel_pub_.publish(vel);
-      vel.angular = 3.0;
-      vel.linear = 3.0;
-      vel_pub_.publish(vel);
-      sleep(1.8);
-
-      vel.angular = 0.0;
-      vel.linear =  0.0;
-      vel_pub_.publish(vel);
-   }
-   else if(strcmp("cercle",msg->data.c_str())==0)
-   {
-      vel.angular = 2.5;
-      vel.linear =  2.2;
-      vel_pub_.publish(vel);
-   }
-}
-
-void CommandSig::CommandLoop()
-{
-   ros::Rate loop_rate(10);
-   while (ros::ok)
-   {
-      ros::spinOnce();
-      loop_rate.sleep();
-   }
-   return;
-}*/
